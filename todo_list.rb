9d506b889e7d5b08f2d0f2a65b8feebcbe58a8da
Joslyn <jrosbrook@gmail.com>
2015-06-19 10:29:27 -0700

    adding items to todo list

--- todo_list.rb
+++ todo_list.rb
@@ -1,16 +1,19 @@
 require "./todo_item"
 
 class TodoList
-  attr_read :name, :todo_items
+  attr_reader :name, :todo_items
 
   def initialize(name)
     @name = name
     @todo_items = []
   end
+
+  def add_item(name)
+    todo_items.push(TodoItem.new(name))
+  end
 end
 
 todo_list = TodoList.new("groceries")
-todo_item = TodoItem.new("milk")
+todo_list.add_item("milk")
 
 puts todo_list.inspect
-puts todo_item.inspect
