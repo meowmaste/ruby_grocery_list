5221cd2912b3247c2d8db883fa1e7fe2d22fb3e5
Joslyn <jrosbrook@gmail.com>
2015-06-19 10:43:18 -0700

    removing todo items

--- todo_list.rb
+++ todo_list.rb
@@ -11,9 +11,41 @@ class TodoList
   def add_item(name)
     todo_items.push(TodoItem.new(name))
   end
+
+
+  def remove_item(name)
+    index = 0
+    found = false
+
+    todo_items.each do |todo_item|
+      if todo_item.name == name
+        found = true
+      end
+      if found
+        break
+      else
+        index += 1
+      end
+    end
+    if found
+      todo_items.delete_at(index)
+      return true
+    else
+      return false
+    end
+  end
+
+  def mark_complete(name)
+
+  end
+
 end
 
 todo_list = TodoList.new("groceries")
 todo_list.add_item("milk")
 
 puts todo_list.inspect
+
+if todo_list.remove_item("eggs")
+  puts "eggs were removed from the list"
+end
