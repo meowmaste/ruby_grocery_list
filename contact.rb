7c7855209f5b07a37136a7b6ed80a79154810905
Joslyn <jrosbrook@gmail.com>
2015-06-21 14:23:33 -0700

    adding new last_first format and to_s method

--- contact.rb
+++ contact.rb
@@ -13,6 +13,18 @@ class Contact
     @last_name
   end
 
+  def last_first
+    last_first = last_name
+    last_first += ", "
+    last_first += first_name
+    if !@middle_name.nil?
+      last_first += " "
+      last_first += middle_name.slice(0,1)
+      last_first += "."
+    end
+    last_first
+  end
+
   def full_name
     full_name = first_name
     if !@middle_name.nil?
@@ -22,6 +34,21 @@ class Contact
     full_name += " "
     full_name += last_name
     full_name
+  end
+
+  def to_s(format = 'full_name')
+    case format
+    when 'full_name'
+      full_name
+    when 'last_name'
+      last_first
+    when 'first'
+      first_name
+    when 'last'
+      last_name
+    else
+      first_last
+    end
   end
 end
 
